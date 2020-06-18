e21b7136aa10d740037431b27adee8e8fa241c14
Geordie Parappilly <geordie.p@hotmail.com>
2020-06-18 14:26:55 -0700

    team list better

--- src/features/teamList/TeamItem.tsx
+++ src/features/teamList/TeamItem.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import styles from './TeamItem.module.css';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import { Button } from '@material-ui/core';
 
-function TeamItem(props: { name: React.ReactNode }) {
+function TeamItem(props: { name: React.ReactNode, record: React.ReactNode }) {
   return (
-    <div className={styles.teamItem}>
-      <li>
-        <span className={styles.dot}></span>&nbsp; {props.name}
-      </li>
+    <div>
+      <Button fullWidth>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <ImageIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={props.name} secondary={props.record} />
+        </ListItem>
+      </Button>
     </div>
   );
 }
