1dc879108cd2ed7e130b049ea348062cb8c3b106
Jimmy Rowland <tian_yu@student.smc.edu>
2020-07-23 22:10:08 -0700

    heroku8 remove env log

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,7 +2,6 @@ import { connect, connection } from "mongoose";
 import "dotenv/config";
 
 export default function connectDatabase() {
-    console.log("process.env",process.env);
     const devConnection: string | undefined = process.env.DB_STRING;
     const prodConnection: string | undefined = process.env.DB_STRING_PROD;
 
