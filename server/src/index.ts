7b56d412dcac118dd59cba5f9046af4b1978f710
Jimmy Rowland <tian_yu@student.smc.edu>
2020-07-24 02:18:36 -0700

    heroku 16 regex

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ import { TestResolver } from "./resolver/TestResolver";
     connectDatabase();
     app.use(
         cors({
-            origin: ["http://localhost:3000","https://teamsporty-2mwwo01ex.vercel.app"],
+            origin: ["http://localhost:3000",/^https:\/\/teamsporty-\w{9}\.vercel\.app$/],
             credentials: true,
 
         }),
