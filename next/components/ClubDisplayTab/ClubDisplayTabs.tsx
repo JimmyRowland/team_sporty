49e3e709cec1fa75006c02b5680e3522c1733b71
WilliamJizh <65744833+WilliamJizh@users.noreply.github.com>
2020-07-24 06:55:05 -0700

    teamsearchpage styling/ lazy search

--- next/components/ClubDisplayTab/ClubDisplayTabs.tsx
+++ next/components/ClubDisplayTab/ClubDisplayTabs.tsx
@@ -5,6 +5,7 @@ import Avatar from "@material-ui/core/Avatar";
 import { Button } from "@material-ui/core";
 import { GTranslate } from "@material-ui/icons";
 import { Team, useApplyTeamMutation } from "../../generated/graphql";
+import Card from "@material-ui/core/Card";
 
 const useStyles = makeStyles((Theme: Theme) =>
     createStyles({
@@ -74,6 +75,7 @@ export default function ClubDisplayTab({
     description,
     teamID,
     isMember,
+    teamimage,
 }: {
     name: string;
     sport: string;
@@ -81,6 +83,7 @@ export default function ClubDisplayTab({
     description: string;
     teamID: string;
     isMember: boolean;
+    teamimage: string;
 }) {
     const classes = useStyles();
     const [joinTeam, loading] = useApplyTeamMutation({ variables: { teamID: teamID } });
@@ -88,9 +91,9 @@ export default function ClubDisplayTab({
         joinTeam();
     };
     return (
-        <div className={classes.body}>
+        <Card className={classes.body}>
             <div className={classes.clubIMGContainer}>
-                <Avatar className={classes.clubIMG}>T</Avatar>
+                <Avatar className={classes.clubIMG} src={teamimage}></Avatar>
             </div>
             <div className={classes.infoContainer}>
                 <div className={classes.infocontainer}>
@@ -107,13 +110,20 @@ export default function ClubDisplayTab({
             </div>
             <div className={classes.addButtonContainer}>
                 {isMember ? (
-                    `${loading} ${isMember}`
+                    <Button disabled variant="contained" color="primary" className={classes.addButton}>
+                        Joined
+                    </Button>
                 ) : (
-                    <Button onClick={handleJoinTeam} variant="contained" color="primary" className={classes.addButton}>
+                    <Button
+                        onClick={handleJoinTeam}
+                        variant="contained"
+                        color="secondary"
+                        className={classes.addButton}
+                    >
                         Join
                     </Button>
                 )}
             </div>
-        </div>
+        </Card>
     );
 }
