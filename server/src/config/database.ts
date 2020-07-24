c91bd044e1275f4a85fec39de2208c700d0996c7
Jimmy Rowland <tian_yu@student.smc.edu>
2020-07-23 21:55:13 -0700

    heroku6 test env variables

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,6 +2,7 @@ import { connect, connection } from "mongoose";
 import "dotenv/config";
 
 export default function connectDatabase() {
+    console.log("process.env",process.env);
     const devConnection: string | undefined = process.env.DB_STRING;
     const prodConnection: string | undefined = process.env.DB_STRING_PROD;
 
