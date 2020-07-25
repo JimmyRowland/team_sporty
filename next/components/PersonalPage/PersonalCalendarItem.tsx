c94a5739041a29818b51f230bd2d4346f1f8281e
Geordie Parappilly <geordie.p@hotmail.com>
2020-07-24 20:25:30 -0700

    fixed bug on personal profile

--- next/components/PersonalPage/PersonalCalendarItem.tsx
+++ next/components/PersonalPage/PersonalCalendarItem.tsx
@@ -1,37 +1,19 @@
 import React from "react";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { DateAvatar } from "../PersonalPage/DateAvatarNew";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            width: "100%",
-            borderRadius: "1rem",
-            minHeight: 414,
-        },
-        media: {
-            height: 0,
-            paddingTop: "56.25%" // 16:9
-        },
-        inline: {
-            display: 'inline',
-        },
-    })
-);
 
 export default function CalendarItem(props: {
     key: React.ReactNode;
     name: React.ReactNode;
     type: React.ReactNode;
-    date: React.ReactNode;
+    date: Date;
     address: React.ReactNode;
     event: React.ReactNode;
     refetch: React.ReactNode;
 }) {
-    const classes = useStyles();
 
     const eventDate = new Date(props.date);
     function formatAMPM(date: Date) {
@@ -40,9 +22,11 @@ export default function CalendarItem(props: {
         var ampm = hours >= 12 ? 'pm' : 'am';
         hours = hours % 12;
         hours = hours ? hours : 12; // the hour '0' should be '12'
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        var strTime = hours + ':' + minutes + ' ' + ampm;
-        return strTime;
+        if (minutes < 10){
+            return hours + ':0' + minutes + ' ' + ampm;
+        } else {
+            return hours + ':' + minutes + ' ' + ampm;
+        }
     }
     const timeString = formatAMPM(eventDate);
 
